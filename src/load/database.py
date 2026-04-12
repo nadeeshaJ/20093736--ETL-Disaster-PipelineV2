131e69f0a10b012d1b5ac4ea1c6752994df9100c
nadeeshaJ <nadeesha729@gmail.com>
2026-04-12 21:55:52 +0100

    database connection added

--- src/load/database.py
+++ src/load/database.py
@@ -1,27 +1,50 @@
-# src/load/database.py
-import os
-from supabase import create_client, Client
-import pandas as pd
 import logging
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import pandas as pd
+from supabase import create_client, Client 
 
 class SupabaseLoader:
     def __init__(self):
-        # These should be in an .env file for security
-        self.url = os.environ.get("SUPABASE_URL")
-        self.key = os.environ.get("SUPABASE_KEY")
+        # Load .env 
+        base_path = Path(__file__).resolve().parent.parent.parent
+        load_dotenv(dotenv_path=base_path / '.env') 
+
+        self.url: str = os.getenv("SUPABASE_URL")
+        self.key: str = os.getenv("SUPABASE_KEY")
+
+        if not self.url or not self.key:
+            raise ValueError("Check your .env file! URL or Key is missing.")
+
+        # Initialize the client
         self.client: Client = create_client(self.url, self.key)
 
     def upload_dataframe(self, df: pd.DataFrame, table_name: str):
-        """Upserts data into Supabase to prevent duplicates."""
+       
         try:
-            # Clean data: Convert NaNs to None for Postgres compatibility
+            # Convert NaNs to None 
             df_clean = df.where(pd.notnull(df), None)
             data = df_clean.to_dict(orient='records')
             
-            # Upsert ensures that if an event_id already exists, it updates instead of failing
+            # Upsert 
             response = self.client.table(table_name).upsert(data).execute()
             logging.info(f"Successfully uploaded {len(data)} rows to {table_name}.")
             return response
         except Exception as e:
             logging.error(f"Database upload failed: {e}")
             raise
+
+    def test_connection(self):
+        try:
+            
+            self.client.table("latest_scorecards").select("*").limit(1).execute()
+            print("Supabase connection successful!")
+            return True
+        except Exception as e:
+            print(f"Connection failed: {e}")
+            return False
+
+if __name__ == "__main__":
+    loader = SupabaseLoader()
+    loader.test_connection()
