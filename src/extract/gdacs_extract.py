52043e10d81297ce3bc4e1f3a60d5549d96eee64
nadeeshaJ <nadeesha729@gmail.com>
2026-04-13 02:31:08 +0100

    gdcs new features added

--- src/extract/gdacs_extract.py
+++ src/extract/gdacs_extract.py
@@ -5,14 +5,13 @@ import requests
 import logging
 
 def gdcs_extraction():
-    
-    logging.info("Downloading GDACS alerts...")
+    logging.info("Downloading advanced GDACS alerts...")
     api_url = "https://www.gdacs.org/gdacsapi/api/events/geteventlist/SEARCH" 
     params = {
         "fromdate": "2025-01-01",
-        "todate": datetime.now().strftime("%Y-%m-%d"),
+        "todate": datetime.now().strftime("%Y-%m-%d"), 
         "alertlevel": "Green,Orange,Red",
-        "format": "json"
+          "format": "json"
     }
     
     response = requests.get(api_url, params=params, timeout=60)
@@ -25,17 +24,18 @@ def gdcs_extraction():
         props = event.get("properties", {})
         geom = event.get("geometry", {})
         coords = geom.get("coordinates", [None, None])
-        
         severity_data = props.get("severitydata", {})
         
         rows.append({
-            "event_id": str(props.get("eventid")), # TEXT in DB
+            "event_id": str(props.get("eventid")),
+            "glide_no": props.get("glideno"), 
             "country": props.get("country"),
             "iso3": props.get("iso3"),
             "disaster_type": props.get("eventtype"),
             "alert_level": props.get("alertlevel"),
             "severity": props.get("severity"),
-            "magnitude": severity_data.get("severity", 0),
+            "magnitude": severity_data.get("severity", 0), # magnitude
+            "magnitude_unit": severity_data.get("severityunit"), # units : Mw or km/h
             "population": props.get("population"),
             "start_date": props.get("fromdate"),
             "end_date": props.get("todate"),
