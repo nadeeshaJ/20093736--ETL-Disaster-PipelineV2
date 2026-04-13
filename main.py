7b56ce5e4bfc2cd69121768e18fc27a2527c2942
nadeeshaJ <nadeesha729@gmail.com>
2026-04-13 11:15:10 +0100

    All pipiline runner created

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import sys
+import os
 import pandas as pd
 from src.load.versioning import rotate_files, save_new_files
 from src.extract.emdat_extract import emdat_extraction
@@ -8,6 +9,7 @@ from src.extract.worldbank_extract import worldbank_extraction
 from src.load.database import SupabaseLoader
 from src.extract.gdacs_extract import gdcs_extraction
 from src.transform.build_scorecard import build_historical_validation, build_scorecard
+
 
 # log functions
 logging.basicConfig(
@@ -48,6 +50,9 @@ def run_historical_pipeline():
         
         
         output_path = "data/outputs/historical_validation.csv"
+       
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        
         historical_df.to_csv(output_path, index=False)
         logging.info(f"Historical data saved locally to {output_path}")
         
@@ -60,13 +65,38 @@ def run_historical_pipeline():
     except Exception as e:
         logging.error(f"Historical pipeline failed: {e}")
 
+def run_full_pipeline():
+    # master function to run all 4 tasks in logical sequence
+    logging.info("Starting full integrated disaster pipeline")
+    
+    # Reference Indicators
+    logging.info("Task 1: World Bank extraction")
+    worldbank_extraction()
+    
+    # Historical Records
+    logging.info("Task 2: EM-DAT extraction")
+    emdat_extraction()
+    
+    # Build Validation Intelligence
+    logging.info("Task 3: Historical validation processing")
+    run_historical_pipeline()
+    
+    # Build Live Scorecards
+    logging.info("Task 4 : Real-time triage processing")
+    run_realtime_pipeline()
+    
+    logging.info("Full pipeline completed successfully")
+
 def main():
     parser = argparse.ArgumentParser(description="MSc Disaster ETL Pipeline")
     
-    parser.add_argument('--task', choices=['realtime', 'worldbank', 'emdat', 'historical'], required=True)
+    
+    parser.add_argument('--task', choices=['realtime', 'worldbank', 'emdat', 'historical', 'all'], required=True)
     args = parser.parse_args()
 
-    if args.task == 'realtime':
+    if args.task == 'all':
+        run_full_pipeline()
+    elif args.task == 'realtime':
         run_realtime_pipeline()
     elif args.task == 'worldbank':
         worldbank_extraction() 
